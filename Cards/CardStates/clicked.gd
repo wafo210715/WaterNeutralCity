0b08b48017367af66e21ed3cec15623ffc7f06ea
Guanhong Li <wafo210715@gmail.com>
2024-11-25 10:41:41 +0000

    version15_1
    delete redundant states

--- Cards/CardStates/clicked.gd
+++
@@ -1,9 +0,0 @@
-extends CardState
-
-func enter():
-	card_ui.drop_point_detector.monitoring = true
-	print("Clicked")
-
-func on_input(event: InputEvent):
-	if event is InputEventMouseMotion:
-		transition_requested.emit(self, CardState.State.DRAGGING)
